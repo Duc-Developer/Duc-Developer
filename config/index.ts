5d0b814ec0d239aef54f58d96416792179b70778
David Chan <66018863+Duc-Developer@users.noreply.github.com>
2024-09-17 23:03:14 +0700

    init portfolio (#1)
    Co-authored-by: unknown <ductt2@vmogroup.com>

---
+++ config/index.ts
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "David Chan | Software Engineer",
+  description: "Welcome to my portfolio! I'm David Chan, a software engineer.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "ci/cd",
+    "react",
+    "fullstack",
+    "frontend",
+    "leader",
+    "senior",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "David Chan",
+    url: "https://github.com/Duc-Developer",
+  },
+} as const;
