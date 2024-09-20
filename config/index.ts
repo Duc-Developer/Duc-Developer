3bc2368053b44bc33d572103d35a97ddba4c1c7d
David Chan <mrtranduc1994@outlook.com>
2024-09-20 10:33:42 +0700

    add metadata

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,34 @@
 import type { Metadata } from "next";
 
-export const siteConfig: Metadata = {
+export const metadataEn = {
   title: "David Chan | Software Engineer",
   description: "Welcome to my portfolio! I'm David Chan, a software engineer.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "ci/cd",
+    "react",
+    "fullstack",
+    "frontend",
+    "leader",
+    "senior",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "David Chan",
+    url: "https://github.com/Duc-Developer",
+  },
+} as const;
+
+export const metadataVn = {
+  title: "David Chan | Kỹ sư phần mềm",
+  description: "Chào mừng đến với portfolio của tôi! Tôi là David Chan, một kỹ sư phần mềm.",
   keywords: [
     "reactjs",
     "nextjs",
