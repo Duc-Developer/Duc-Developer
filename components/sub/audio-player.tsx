813dbff7f3726e7f316498df98769fdfd4646488
David Chan <mrtranduc1994@outlook.com>
2024-10-02 15:49:35 +0700

    change audio music

--- components/sub/audio-player.tsx
+++ components/sub/audio-player.tsx
@@ -3,8 +3,19 @@ import { throttle } from '@/lib/utils';
 import React, { useEffect, useRef, useState } from 'react';
 import { FaPlay, FaPause, FaDownload, FaRedo } from "react-icons/fa";
 
-const audioLink = "https://res.cloudinary.com/drwgmpzuh/video/upload/v1727628590/david.id.vn/lofi-song-jinsei-by-lofium-236730_fph5mh.mp3";
+const audioLink = "https://res.cloudinary.com/drwgmpzuh/video/upload/v1727858091/david.id.vn/avangard_phonk.mp3";
 const backgroundUrl = "https://res.cloudinary.com/drwgmpzuh/image/upload/v1727670340/david.id.vn/3d-rendering-astronaut_23-2151432351_rhcdd5.webp";
+
+const AUDIO = {
+    title: "Avangard Phonk",
+    artist: "Remix by Avangard",
+    mapper: "Avangard",
+    likes: '2k',
+    plays: '75k',
+    date: "25 Sep 2024",
+    link: audioLink
+};
+
 const AudioPlayer = () => {
     const audioRef = useRef<HTMLAudioElement>(null);
     const [isPlaying, setIsPlaying] = useState(false);
@@ -24,7 +35,7 @@ const AudioPlayer = () => {
     const handleDownload = async (e: any) => {
         e.stopPropagation();
         try {
-            const response = await fetch(audioLink);
+            const response = await fetch(AUDIO.link);
             const blob = await response.blob();
             const url = window.URL.createObjectURL(blob);
             const link = document.createElement('a');
@@ -101,11 +112,11 @@ const AudioPlayer = () => {
                     </div>
                     <div className="absolute inset-0 flex flex-col justify-between p-4 text-white">
                         <div className="space-y-1">
-                            <div className="text-3xl font-medium">Lofi Song</div>
-                            <div className="font-medium">Jinsei by Lofium</div>
+                            <div className="text-3xl font-medium">{AUDIO.title}</div>
+                            <div className="font-medium">{AUDIO.artist}</div>
                             <div className="text-sm">
                                 mapped by {" "}
-                                <span className="text-darkNeutral italic transition-all hover:text-purple">Lofium</span>
+                                <span className="text-darkNeutral italic transition-all hover:text-purple">{AUDIO.mapper}</span>
                             </div>
                         </div>
                         <div className="invisible flex space-x-3 opacity-0 transition-all group-hover:visible group-hover:opacity-100">
@@ -113,20 +124,20 @@ const AudioPlayer = () => {
                                 <svg className="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                                 </svg>
-                                <div>33</div>
+                                <div>{AUDIO.likes}</div>
                             </span>
                             <span className="flex items-center space-x-1">
                                 <svg className="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
-                                <div>75.7k</div>
+                                <div>{AUDIO.plays}</div>
                             </span>
                             <span className="flex items-center space-x-1">
                                 <svg className="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
-                                <div>25 Mar 2022</div>
+                                <div>{AUDIO.date}</div>
                             </span>
                         </div>
                         <div className="flex space-x-2">
