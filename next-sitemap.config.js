217c5d6cf2ee59111862f44a6685ecc150840f89
David Chan <mrtranduc1994@outlook.com>
2024-10-06 13:45:21 +0700

    handle sitemap by app folder

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-const domain = process.env.DOMAIN;
-module.exports = {
-    siteUrl: domain,
-    generateRobotsTxt: true,
-    generateIndexSitemap: false,
-    outDir: "./dist/server/pages",
-    output: 'export'
-}
