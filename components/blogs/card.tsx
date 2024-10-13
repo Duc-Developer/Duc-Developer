35d500101d7fceb345f3c348211b7f9c37e4bcbd
David Chan <mrtranduc1994@outlook.com>
2024-10-13 21:09:34 +0700

    faet: update layout for card blog

--- components/blogs/card.tsx
+++ components/blogs/card.tsx
@@ -3,29 +3,29 @@ import styles from './card.module.css'
 import { blogger_v3 } from 'googleapis';
 import { sanitizeDescription, SlugConverter } from '@/utilities';
 import Link from 'next/link';
-import { FaArrowRight } from "react-icons/fa";
+import { showToast } from '../common/toast';
+import { FaShareAlt } from 'react-icons/fa';
 
 const PostCard = ({ data }: { data: blogger_v3.Schema$Post }) => {
     const cleanedContent = sanitizeDescription(data);
+    const handleShareClick = async () => {
+        if (navigator.share) {
+            try {
+                await navigator.share({
+                    title: document.title,
+                    text: 'Check out this post!',
+                    url: window.location.href,
+                });
+                showToast({ message: 'Content shared successfully', status: 'success' });
+            } catch (error) {
+                showToast({ message: 'Share canceled', status: 'error' });
+            }
+        } else {
+            showToast({ message: 'Web Share API not supported in this browser' });
+        }
+    };
     return <div className={styles.wrapper}>
         <div className={styles.cardHover}>
-            <div className={styles.cardHoverBackground} />
-            <div className={styles.cardHoverContent}>
-                <h3 className={styles.cardHoverTitle}>
-                    {data.title}
-                </h3>
-                <p className={styles.cardHoverText}>
-                    <span>{cleanedContent}</span>
-                </p>
-                <Link href={`blogs/${SlugConverter.toPostSlug(data.url) ?? '#not-found'}`} className={styles.cardHoverLink}>
-                    <span>Xem tiếp</span>
-                    <FaArrowRight />
-                </Link>
-            </div>
-            <div className={styles.cardHoverExtra}>
-                <h4 className={styles.cardHoverTextExtra}>Nếu thấy hay đừng quên <span>like</span> và <span>share</span> cho mình nhé!</h4>
-                <i>www.codecungdavid.blogspot.com</i>
-            </div>
             <Image
                 className={styles.cardHoverThumbnail}
                 src={data.images?.[0].url ?? ''}
@@ -33,6 +33,26 @@ const PostCard = ({ data }: { data: blogger_v3.Schema$Post }) => {
                 width={200}
                 height={150}
             />
+            <div className={styles.cardHoverContent}>
+                <h3 className={styles.cardHoverTitle} title={data.title ?? ''}>
+                    {data.title}
+                </h3>
+                <p className={styles.cardHoverText}>
+                    <span>{cleanedContent}</span>
+                </p>
+            </div>
+            <div className={styles.cardHoverFooter}>
+                <Link
+                    href={`blogs/${SlugConverter.toPostSlug(data.url) ?? '#not-found'}`}
+                    className={styles.cardHoverLink}
+                    data-replace="Đọc tiếp"
+                >
+                    <span>Đọc tiếp</span>
+                </Link>
+                <button className={styles.shareButton} onClick={handleShareClick} title='share'>
+                    <FaShareAlt className={styles.shareIcon} size={18} />
+                </button>
+            </div>
         </div>
     </div>;
 }
