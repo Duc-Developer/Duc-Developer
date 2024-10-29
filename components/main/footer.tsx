cc50a41a976768f37996324255c25a5cc11a7347
David Chan <mrtranduc1994@outlook.com>
2024-10-29 23:07:39 +0700

    responsive for mobile blog content

--- components/main/footer.tsx
+++ components/main/footer.tsx
@@ -1,9 +1,16 @@
 import { useTranslation } from "@/hooks/useTranslation";
+import { classNames } from "@/lib/utils";
+import { useRouter } from "next/router";
 
 export const Footer = () => {
   const { t } = useTranslation('common');
+  const { pathname } = useRouter();
+  const isPostContent = /\/blogs\/.*/.test(pathname);
   return (
-    <section className="text-center my-5">
+    <section className={classNames(
+      "text-center my-5",
+      isPostContent ? "hidden sm:block" : "block"
+    )}>
       <h5 className="select-none">
         {t('footer_copyright', { suffix: "© 2024 David Chan." })}
       </h5>
